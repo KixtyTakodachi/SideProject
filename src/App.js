a7989dc8cc70cac4415d4806ff3d9fe3f6e2de3f
Kixty <sanya.kim.sk@gmail.com>
2022-11-25 19:37:13 +0600

    Login page added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Login from "./Components/Login/Login";
+import Themes from "./Components/Themes/Themes";
+import ThemeItem from "./Components/ThemeItem/ThemeItem";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Login />}/>
+            <Route path="/login" element={<Login />}/>
+            <Route path="/themes" element={<Themes />}/>
+            <Route path="/theme" element={<ThemeItem />}/>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react'
+import './Login.scss'
+import {Form, Input, Button, Checkbox} from "antd";
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+export default function Login(){
+
+    const [isError, setIsError] = useState(false)
+    const usersList = [
+        {
+            login: 'test',
+            password: 'test123'
+        }
+    ]
+
+    const submitForm = (values) => {
+        const user = usersList.find(item => item.login == values.login)
+        if(!user || user.password !== values.password){
+            setIsError(true)
+        } else {
+            window.location.replace(window.location.origin + '/themes')
+        }
+    }
+
+
+    return (
+        <div className='login_bg'>
+            <div className='login_form'>
+                <h1 className='login_title'>
+                    Вход
+                </h1>
+                <div className={['login_error', isError ? 'active' : ''].join(' ')}>
+                    Неверное имя пользователя или пароль
+                </div>
+                <Form
+                    className='form'
+                    onFinish={submitForm}
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                >
+                    <Form.Item name='login' rules={[{required: true, message:'Введите логин!'}]}>
+                        <Input className='form_input' prefix={<UserOutlined />} placeholder='Логин'/>
+                    </Form.Item>
+                    <Form.Item name='password' rules={[{required: true, message: 'Ввеидте пароль!'}]}>
+                        <Input.Password className='form_input' prefix={<LockOutlined />} placeholder='Пароль'/>
+                    </Form.Item>
+                    <Form.Item name='remember_me' valuePropName='checked'>
+                        <Checkbox defaultChecked={false}> Запомнить меня </Checkbox>
+                    </Form.Item>
+                    <Button htmlType='submit' type='primary' className='form_button'>Вход</Button>
+                </Form>
+            </div>
+        </div>
+    )
+}
